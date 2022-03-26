e1a38aec5802a3b89e7240965b3c53b83ac8bad6
MatasRutkauskas <matas.rutkauskas02@gmail.com>
2022-03-26 15:56:42 +0200

    Setup and connect docker db image, add typescript support

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# back-end
+# Project setup
+Open `back-end` directory in terminal
+
+Run `npm install`
+
+Run `docker-compose -p masketplace-db up -d`
+
+Make sure no errors occured and you can see `mongodb` and `mong-express` containers running
+
+Sometimes mongo-express might not connect on first try in that case you should try to activate it again with `docker-compose up mongo-express`
+
+Go to `http://localhost:8081`, create a new database called `masketplace`
+# Starting project
+To start the project simply run `npm run serve`
+
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const { MONGODB_HOSTNAME, MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_PORT } = process.env;
+
+mongoose.connect(
+    `mongodb://${MONGODB_HOSTNAME}:${MONGODB_PORT}`,
+    {
+        dbName: 'masketplace',
+        user: MONGODB_USERNAME,
+        pass: MONGODB_PASSWORD,
+    },
+    (err: any) => {
+        err ? console.info(err) : console.info('connected to database');
+    }
+);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.info(`server has started on ${PORT}`));
