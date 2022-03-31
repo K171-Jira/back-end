a90e902f4cddba5eb1f820c4dd86294a35329c4d
ADaunys <augustas.daunys@ktu.edu>
2022-03-31 13:04:05 +0300

    Login functionality WITHOUT session

--- src/server.ts
+++ src/server.ts
@@ -26,17 +26,40 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.info(`server has started on ${PORT}`));
 
+// create a schema that contains: unique email, password and role
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-    isAdmin: Boolean
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
 });
 const User = mongoose.model('User', userSchema);
 
 app.post('/api/register', async (req, res) => {
-    const { email, password } = req.body;
-    const user = new User({ email, password, isAdmin:false });
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = new User({ email, password, role: 'user' });
     await user.save((err: any) => {
         err ? res.status(500).send(err) : res.status(200).send('user registered');
     });
+});
+
+app.post('/api/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = await User.findOne({ email, password });
+    if (!user) {
+        res.status(401).send('invalid email or password');
+    } else {
+        res.status(200).send(user);
+    }
 });
