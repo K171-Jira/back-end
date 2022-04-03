edc01e2fee86dc321a160a05576576f023681a72
ADaunys <augustas.daunys@ktu.edu>
2022-04-03 16:45:01 +0300

    Register and login functionality with access tokens

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+const auth = require("./middleware/auth");
+import usersRoute from './auth/routes/users';
 import express from 'express'
 const mongoose = require('mongoose')
 import cors from 'cors'
@@ -22,44 +24,11 @@ mongoose.connect(
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/users', usersRoute);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.info(`server has started on ${PORT}`));
 
-// create a schema that contains: unique email, password and role
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    },
-});
-const User = mongoose.model('User', userSchema);
-
-app.post('/api/register', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const user = new User({ email, password, role: 'user' });
-    await user.save((err: any) => {
-        err ? res.status(500).send(err) : res.status(200).send('user registered');
-    });
-});
-
-app.post('/api/login', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const user = await User.findOne({ email, password });
-    if (!user) {
-        res.status(401).send('invalid email or password');
-    } else {
-        res.status(200).send(user);
-    }
+app.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌");
 });
