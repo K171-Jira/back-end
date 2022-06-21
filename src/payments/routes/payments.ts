ca0e97a172002990f47c0d9075e8952777f2a168
Aurvas <79143711+Aurvas@users.noreply.github.com>
2022-06-21 15:49:08 +0300

    Update payments.ts

--- src/payments/routes/payments.ts
+++ src/payments/routes/payments.ts
@@ -5,10 +5,11 @@ import User from '../../auth/models/user';
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 
 router.post('/', async (req, res) => {
-	let { id, _id, balance } = req.body
+	let { id, _id, balance, selectedAmount } = req.body
 	let customer: any;
 	const user = await User.findById(_id);
-	const amount = 1000;
+	let amount = selectedAmount*100;
+	console.log('SUMA   ',req.body)
 	try {
 		console.log(user.stripe_id + "AAAAAA");
 		if(!user.stripe_id){
@@ -39,7 +40,7 @@ router.post('/', async (req, res) => {
 			setup_future_usage: "on_session",
 			confirm: true
 		})
-		var newBalance= balance+(amount/100);
+		var newBalance= balance+Math.trunc((amount/100));
 		user.balance=newBalance;
 		const savedUser = await user.save();
 		console.log("Payment", payment)
